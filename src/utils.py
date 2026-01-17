f57b6bb0e47510995a6748b0ec38a56830d67839
Himanshu Sharma <himanshuchobiya@gmail.com>
2026-01-17 23:30:10 +0530

    Hyperparamter Training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 from src.exception import CustomException
@@ -23,24 +24,37 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
         
-def evaluate_models(X_train,X_test,y_train,y_test,models):
+def evaluate_models(X_train, X_test, y_train, y_test, models, params):
     try:
-        report={}
+        report = {}
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+            para = params[model_name]
             
-            model.fit(X_train,y_train) #Train model
+            # Check if it's CatBoost - skip GridSearchCV
+            if 'CatBoost' in model_name:
+                # Train CatBoost directly without GridSearchCV
+                model.fit(X_train, y_train, verbose=False)
+            else:
+                # Use GridSearchCV for other models
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
             
+            # Make predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
             
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
         
         return report
     
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
